50e7fe9714fe4f92ebed8c96742198fdb84fbd27
Cesar Estrada <cesar.estrada585@my.tccd.edu>
2018-09-16 12:44:40 -0400

    update on pizza maker

--- Labs/Lab 1 Pizza Creator/Lab 1 Pizza Creator/Program.cs
+++ Labs/Lab 1 Pizza Creator/Lab 1 Pizza Creator/Program.cs
@@ -9,7 +9,7 @@ namespace Lab_1_Pizza_Creator
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{			
 			bool notQuit;
 			do
 			{
@@ -21,28 +21,28 @@ namespace Lab_1_Pizza_Creator
 		{
 			while (true)
 			{
-				Console.WriteLine("N)ew Order");
-				Console.WriteLine("M)odify Order");
-				Console.WriteLine("D)isplay Order");
-				Console.WriteLine("Q)uit");
+				Console.WriteLine("Please Select one of the menu options.\n");
+				Console.WriteLine("\t1. New Order");
+				Console.WriteLine("\t2. Modify Order");
+				Console.WriteLine("\t3. Display Order");
+				Console.WriteLine("\t4. Quit");
 
 				string input = Console.ReadLine();
 				switch (input[0])
 				{
-					case 'n':
-					case 'N': NewOrder();
+					case '1':
+						NewOrder();
 						return true;
 
-					case 'm':
-					case 'M': ModifyOrder();						
+					case '2':
+						ModifyOrder();						
 						return true;
 
-					case 'd':
-					case 'D': DisplayOrder();						
+					case '3':
+						DisplayOrder();						
 						return true;
 					
-					case 'q':
-					case 'Q':
+					case '4':
 						return false;
 
 					default:
@@ -53,9 +53,39 @@ namespace Lab_1_Pizza_Creator
 			};
 		}
 
-		private static void NewOrder()
+		private static bool NewOrder()
 		{
-			Console.WriteLine("New Order\n");
+			while (true)
+			{
+				Console.WriteLine("Choose the size of your pizza\n");
+				Console.WriteLine("\t1. Small  ($5.00)");
+				Console.WriteLine("\t2. Medium ($6.25)");
+				Console.WriteLine("\t3. Large  ($8.75)");
+				Console.WriteLine("\t4. Return to main menu");
+				
+				string input = Console.ReadLine();
+				switch (input[0])
+				{
+					case '1':
+						SmallPizza();
+						return true;
+
+					case '2':
+						MediumPizza();
+						return true;
+
+					case '3':
+						LargePizza();
+						return true;
+
+					case '4':						
+						return true;
+
+					default:
+						Console.WriteLine("Please enter a valid value.");
+						break;
+				};
+			}
 		}
 		
 		private static void ModifyOrder()
@@ -65,9 +95,84 @@ namespace Lab_1_Pizza_Creator
 
 		private static void DisplayOrder()
 		{
-			Console.WriteLine("Display Order\n");
+			if (String.IsNullOrEmpty(pizzaChoice))
+			{
+				Console.WriteLine("No order is available to view at this time.");
+				return;
+			};
 		}
 
+		private static bool SmallPizza()
+		{
+			int count = 4;
+			string[] meatChoices = { "Bacon", "Ham", "Pepperoni", "Sausage" };
+			while (true)
+			{
+				
+				Console.WriteLine("Select your toppings for your SMALL PIZZA\n");
 
+				
+				Console.WriteLine("Meats (each option is $0.75)\n");
+				Console.WriteLine("\t1. Bacon");
+				Console.WriteLine("\t2. Ham");
+				Console.WriteLine("\t3. Pepperoni");
+				Console.WriteLine("\t4. Sausage");
+				Console.WriteLine("\t5. Done with meat toppings");
+
+				string input = Console.ReadLine();
+				// you may not need line above switch
+				//int number;
+				//Int32.TryParse(input, out number); // this converts to an int 
+				//Console.WriteLine($"{number}\n");
+				// just experimenting here ^^^^^^^^^^^^^^^^^^^^^^^
+				for (int index = 0; index < count; ++index)
+				{
+					input = meatChoices[index];
+					Console.WriteLine(meatChoices[index]);
+				};
+				switch (input[0])
+				{
+					case '1':
+						//SmallPizza();
+						return true;
+
+					case '2':
+						return true;
+
+					case '3':
+						return true;
+
+					case '4':
+						return true;
+
+					case '5':
+						return true;
+
+					default:
+						Console.WriteLine("Please enter a valid value.");
+						break;
+				};
+
+			}
+		}
+
+		private static void MediumPizza()
+		{
+			Console.WriteLine("Medium Pizza\n");
+		}
+
+		private static void LargePizza()
+		{
+			Console.WriteLine("Large Pizza\n");
+		}
+
+		//private static void meats()
+		//{
+		//	SmallPizza();
+		//}
+
+		int pizzaCount = 4;
+		static string pizzaChoice;
+		
 	}
 }

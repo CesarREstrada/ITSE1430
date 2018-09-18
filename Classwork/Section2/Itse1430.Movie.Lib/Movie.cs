0f90643fa4976e44886bb7772a7b3e7b5a48c3d0
CesarREstrada <cesar.estrada585@my.tccd.edu>
2018-09-17 19:05:00 -0500

    Beginning of section 2

---
+++ Classwork/Section2/Itse1430.Movie.Lib/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.Movie.Lib
+{
+    class Movie
+    {
+        public string name;
+        //public System.String Name;
+    }
+}

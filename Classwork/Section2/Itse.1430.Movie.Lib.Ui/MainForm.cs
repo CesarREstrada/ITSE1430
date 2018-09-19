e77d5cda7a5da4f78108412c03451f0f17296a38
CesarREstrada <cesar.estrada585@my.tccd.edu>
2018-09-19 18:48:13 -0500

    section 2 working on form dialog box

---
+++ Classwork/Section2/Itse.1430.Movie.Lib.Ui/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Itse._1430.Movie.Lib.Ui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // file - exit
+        private void eExitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            // this logic only works with the file exit menu to close the window for asking the user before closing
+            // the if statment asks the user wants to close the window
+            if (MessageBox.Show("Are you sure you want to exit?", "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            // closes the form when user selects file exit
+            Close(); 
+        }
+
+        // help - about
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            
+            // Sorry is the message in the box. Help is the title of the pop of box.
+            // this and message box icon have to be in that order the window will center on the main(parent) window
+            MessageBox.Show(this, "Sorry", "Help", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
--- Classwork/Section2/Itse.1430.Movie.Lib.Ui/Program.cs
+++ Classwork/Section2/Itse.1430.Movie.Lib.Ui/Program.cs
@@ -16,7 +16,7 @@ namespace Itse._1430.Movie.Lib.Ui
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
